84bace23f56134fb4f722eb0fe32cacfd3388767
michal-Q <borowiak.michalq@gmail.com>
2024-09-02 18:32:27 +0200

    3

--- To Do List/Program.cs
+++ To Do List/Program.cs
@@ -9,7 +9,7 @@ namespace To_Do_List
 {
     internal static class Program
     {
-        static Mutex mutex = new Mutex(true, "{A822B10D-6C33-4F43-AB9C-EE4719E5B9C4}"); // Unique identifier
+        //static Mutex mutex = new Mutex(true, "{A822B10D-6C33-4F43-AB9C-EE4719E5B9C4}"); // Unique identifier
 
         /// <summary>
         /// The main entry point for the application.
@@ -17,17 +17,17 @@ namespace To_Do_List
         [STAThread]
         static void Main()
         {
-            if (mutex.WaitOne(TimeSpan.Zero, true))
-            {
+            //if (mutex.WaitOne(TimeSpan.Zero, true))
+            //{
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new ToDoList());
-                mutex.ReleaseMutex();
-            }
-            else
-            {
-                MessageBox.Show("Application is already running.");
-            }
+                //mutex.ReleaseMutex();
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Application is already running.");
+            //}
         }
     }
 }
